e10db51ffb43ca50408bebf611e078124db7580a
김아현 <dhflxhdxhd@gmail.com>
2023-11-10 15:29:59 +0900

    Feat : tab component #6

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-import TheHeader from './components/layout/TheHeader.vue';
-import TheFooter from './components/layout/TheFooter.vue';
-import PlanList from './components/PlanList.vue';
+// import TheHeader from './components/layout/TheHeader.vue';
+// import TheFooter from './components/layout/TheFooter.vue';
+// import PlanList from './components/PlanList.vue';
+import VTab from './components/VTab.vue';
 </script>
 
 <template>
   <!-- <TheHeader/>
   <TheFooter/> -->
-  <PlanList/>
+  <!-- <PlanList/> -->
+  <VTab/>
 </template>
 
 <style scoped>
