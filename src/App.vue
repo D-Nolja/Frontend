3ecebaab559d07076e746ecd445b9caccd4bffb0
김아현 <dhflxhdxhd@gmail.com>
2023-11-10 15:59:10 +0900

    Feat : VPlaceCard small #8

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 // import TheHeader from './components/layout/TheHeader.vue';
 // import TheFooter from './components/layout/TheFooter.vue';
 // import PlanList from './components/PlanList.vue';
-import VTab from './components/VTab.vue';
+// import VTab from './components/VTab.vue';
+import VPlaceCardBig from './components/VPlaceCardSmall.vue';
 </script>
 
 <template>
   <!-- <TheHeader/>
   <TheFooter/> -->
   <!-- <PlanList/> -->
+  <VPlaceCardBig/>
   <VTab/>
 </template>
 
