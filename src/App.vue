60039fe74900c1825c85aa58d245fd831c871143
김아현 <dhflxhdxhd@gmail.com>
2023-11-10 14:41:40 +0900

    Feat : planListItem 컴포넌트 분리 및 props 정의

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TheHeader from './components/layout/TheHeader.vue';
 import TheFooter from './components/layout/TheFooter.vue';
+import PlanList from './components/PlanList.vue';
 </script>
 
 <template>
-  <TheHeader/>
-  <TheFooter/>
+  <!-- <TheHeader/>
+  <TheFooter/> -->
+  <PlanList/>
 </template>
 
 <style scoped>
