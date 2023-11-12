64ec2ecb69dac5e865218e3ed152e6c2aae6d50d
or_hyn <dhflxhdxhd@gmail.com>
2023-11-12 15:34:12 +0900

    Feat : sideNavbar 구조 및 스타일 #12

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -1,28 +1,30 @@
 <script setup>
-import {ref} from "vue";
+import {
+    ref
+} from "vue";
 const btnColor = ref("#4285F4");
 const btnSize = ref();
 const fontColor = ref("#fff");
 
-const props = defineProps({
-    size : {
-        type : String,
+const props = defineProps({ 
+    size: {
+        type: String,
         default () {
             "default"
         }
-    }, 
-    color :  String,
-    text : String
+    },
+    color: String,
+    text: String
 })
+
 
 console.log(props.color)
 
-if(props.color === "white"){
+if (props.color === "white") {
     console.log("d")
     btnColor.value = "#fff";
     fontColor.value = "#333"
 }
-
 
 // text : String,
 //     color : String, 
@@ -30,14 +32,13 @@ if(props.color === "white"){
 </script>
 
 <template>
-    <Button :style = "{backgroundColor : btnColor , color : fontColor} " >{{ text }}</Button>
+<Button :style="{backgroundColor : btnColor , color : fontColor} ">{{ text }}</Button>
 </template>
 
 <style scoped>
-Button{
-    width: 4.44625rem;
-    height: 2.75244rem;
-    flex-shrink: 0;
+Button {
+    width: 6.5625rem;
+    height: 4.0625rem;
     border-radius: 10px;
     background: #4285F4;
     border: none;
@@ -45,8 +46,9 @@ Button{
     font-size: 1.17944rem;
     font-style: normal;
     font-weight: 600;
-    line-height: 1.72888rem; /* 146.583% */
+    line-height: 1.72888rem;
     letter-spacing: 0.03538rem;
     box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
+    margin: 5px 0;
 }
 </style>
---
+++ src/components/layout/TheSideNav.vue
@@ -0,0 +1,48 @@
+<script setup>
+import VButton from '../VButton.vue';
+
+import {ref} from "vue";
+
+const days = ref([1,2,3]);
+
+</script>
+
+<template >
+    <nav class="sideNav">   
+        <div>
+        <img src="../../assets/img/avatar.svg" alt="icon" id="logo">
+        <VButton text="전체일정"/>
+            <VButton v-for="day in days" :key="day" :text="`${day}일차`" color="white"></VButton>
+        </div>
+        <div>
+            <VButton text="편집"/>
+            <VButton text="다음"/>
+        </div>
+    </nav>
+</template>
+
+<style sco>
+
+#logo{
+    width: 5rem;
+    height: 5rem;
+    align-items: center;
+    margin: 0 auto;
+}
+.sideNav{
+    padding: 10px 0;
+    width: 8.25rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    border-right: 1.5px solid #EAEAEA;
+    align-items: center;
+    justify-content: space-between;
+}
+
+div{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
