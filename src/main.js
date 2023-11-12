86f2b3295867c4bccab8658329685271d0e860b6
or_hyn <dhflxhdxhd@gmail.com>
2023-11-12 14:38:56 +0900

    Style : 초기화

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import "./assets/main.css";
 import "ant-design-vue/dist/reset.css";
+import "./assets/main.css";
 import Antd from "ant-design-vue";
 import { createApp } from "vue";
 // import { createPinia } from 'pinia'
