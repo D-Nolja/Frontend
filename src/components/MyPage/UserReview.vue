23f093bceef27a0a2241f496c7cd3d12528a7217
or_hyn <dhflxhdxhd@gmail.com>
2023-11-14 03:56:09 +0900

    Feat : UserReview, Item 구조

--- src/components/MyPage/UserReview.vue
+++ src/components/MyPage/UserReview.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>여행 후기</div>
+  <div class="review-card-container">
+    <user-review-item></user-review-item>
+  <user-review-item></user-review-item>
+  <user-review-item></user-review-item>
+  <user-review-item></user-review-item>
+</div>
+ 
 </template>
 
 <script setup>
-
+import UserReviewItem from './Item/UserReviewItem.vue';
 </script>
 
 <style scoped>
-
+.review-card-container{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 </style>
