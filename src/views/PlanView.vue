a34b23653817b801af498f06f50e4cf83240a197
or_hyn <dhflxhdxhd@gmail.com>
2023-11-13 23:16:28 +0900

    Fix : 경로 수정

--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TheSideNav from '../components/layout/TheSideNav.vue';
-import SideMenu from '../components/SideMenu.vue';
-import VMap from '../components/VMap.vue';
+import SideMenu from '../components/SideMenu/SideMenu.vue';
+import VMap from '../Maps/VMap.vue';
 </script>
 
 <template>
