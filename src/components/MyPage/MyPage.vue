df3b8a2c091c673a94f3d7b6aeae31f3b401afb8
or_hyn <dhflxhdxhd@gmail.com>
2023-11-13 23:28:20 +0900

    Feat : main 화면 구조 구성

---
+++ src/components/MyPage/MyPage.vue
@@ -0,0 +1,20 @@
+
+
+<script setup>
+import VTab from "@/components/Common/VTab.vue"
+</script>
+
+<template>
+<div class="contents-container"> 
+    <v-tab></v-tab>
+</div>
+</template>
+
+<style scoped>
+.contents-container{
+  width: 100%;
+  align-items: center;
+  margin: 80px auto;
+  height: 70vh;
+}
+</style>
