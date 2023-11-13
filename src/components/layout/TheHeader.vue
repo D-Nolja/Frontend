f47226bb10fb28e82d9530c7c9694c2bff327e67
or_hyn <dhflxhdxhd@gmail.com>
2023-11-13 21:29:21 +0900

    Style : header 효과 추가

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -19,6 +19,19 @@
 </template>
 
 <style scoped>
+/* 마우스 호버 및 포커스 상태를 위한 스타일 */
+.nav-btn-container div:hover, .nav-btn-container div:focus {
+    color: #666; /* 호버 또는 포커스 시 색상 변경 */
+    cursor: pointer;
+}
+
+/* 클릭 이벤트를 위한 액티브 클래스 */
+.nav-btn-container div:active {
+    transform: scale(0.95); /* 클릭 시 약간 축소 효과 */
+    color: #4A4A4A; /* 클릭 시 색상 변경 */
+    transition: transform 0.1s, color 0.1s; /* 부드러운 전환 효과 */
+}
+
 .nav-container{
     box-sizing: border-box;
     display : flex;
