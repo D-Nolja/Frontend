480ad329747d5cf396c96265aaa36c31a75b3f36
or_hyn <dhflxhdxhd@gmail.com>
2023-11-14 14:32:25 +0900

    Feat : Input, Submit Button 컴포넌트 분리 #27

---
+++ src/components/Common/VInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <input type="text" name="" id="" :placeholder="txt">
+</template>
+
+<script setup>
+
+defineProps({
+    txt : String
+})
+</script>
+
+<style scoped>
+input{
+    width: 23.0625rem;
+    height: 3.875rem;
+    flex-shrink: 0;
+    border-radius: 0.5rem;
+    background-color: #E6EFFF;
+    border: none;
+    padding: 15px;
+    margin-bottom: 20px;
+}
+
+input::placeholder{
+    /*  text style  */
+    color: #8AB4FF;
+    font-family: Poppins;
+    font-size: 0.9375rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+input:focus {
+    border: 1.5px solid #9cbcf5; 
+    outline: none;
+    box-shadow: 0 0 5px #9cbcf5; 
+}
+</style>
