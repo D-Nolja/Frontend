76bd128c6eb2c00b559c78ff94fe406c5248f787
or_hyn <dhflxhdxhd@gmail.com>
2023-11-19 12:32:31 +0900

    Feat : joinView 생성

---
+++ src/views/JoinView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TheHeader from "@/components/layout/TheHeader.vue";
+import TheFooter from "@/components/layout/TheFooter.vue";
+import VJoinBox from "../components/Auth/VJoinBox.vue";
+</script>
+
+<template>
+  <div class="contents-container">
+    <the-header></the-header>
+
+    <VJoinBox />
+    <the-footer></the-footer>
+  </div>
+</template>
+
+<style scoped>
+.contents-container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
