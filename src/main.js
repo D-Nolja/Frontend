9dce2d36028c6194b16250eb96bb23be72c5287c
or_hyn <dhflxhdxhd@gmail.com>
2023-11-19 12:29:24 +0900

    Setting : ... #37

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import "ant-design-vue/dist/reset.css";
 import "./assets/main.css";
 import "@/assets/text-truncate-util.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Antd from "ant-design-vue";
 import { createApp } from "vue";
-// import { createPinia } from 'pinia'
+import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
-// app.use(createPinia());
+app.use(createPinia());
 app.use(router);
 
 app.use(Antd).mount("#app");
+app.AOS = new AOS.init();
