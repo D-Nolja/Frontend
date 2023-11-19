7afefbba0c2e596cabe24f74ba64c3404adefcd8
or_hyn <dhflxhdxhd@gmail.com>
2023-11-19 12:31:06 +0900

    Feat : login axios 연결

--- src/components/Common/VInput.vue
+++ src/components/Common/VInput.vue
@@ -1,39 +1,51 @@
 <template>
-  <input type="text" name="" id="" :placeholder="txt">
+  <input
+    type="text"
+    id=""
+    :placeholder="txt"
+    :value="modelValue"
+    @input="onInput"
+  />
 </template>
 
 <script setup>
-
 defineProps({
-    txt : String
-})
+  txt: String,
+  modelValue: String,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const onInput = (event) => {
+  emit("update:modelValue", event.target.value);
+};
 </script>
 
 <style scoped>
-input{
-    width: 23.0625rem;
-    height: 3.875rem;
-    flex-shrink: 0;
-    border-radius: 0.5rem;
-    background-color: #E6EFFF;
-    border: none;
-    padding: 15px;
-    margin-bottom: 20px;
+input {
+  width: 23.0625rem;
+  height: 3.875rem;
+  flex-shrink: 0;
+  border-radius: 0.5rem;
+  background-color: #e6efff;
+  border: none;
+  padding: 15px;
+  margin-bottom: 20px;
 }
 
-input::placeholder{
-    /*  text style  */
-    color: #8AB4FF;
-    font-family: Poppins;
-    font-size: 0.9375rem;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
+input::placeholder {
+  /*  text style  */
+  color: #8ab4ff;
+  font-family: Poppins;
+  font-size: 0.9375rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 }
 
 input:focus {
-    border: 1.5px solid #9cbcf5; 
-    outline: none;
-    box-shadow: 0 0 5px #9cbcf5; 
+  border: 1.5px solid #9cbcf5;
+  outline: none;
+  box-shadow: 0 0 5px #9cbcf5;
 }
 </style>
