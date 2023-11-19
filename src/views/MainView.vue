318d203a64c646584dace3f5bb262029d8355ad0
or_hyn <dhflxhdxhd@gmail.com>
2023-11-19 12:32:16 +0900

    Feat : header(navbar) 상태관리 분리

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,28 +1,22 @@
 <script setup>
-
 import TheHeader from "@/components/layout/TheHeader.vue";
 import TheFooter from "@/components/layout/TheFooter.vue";
 import VMain from "../components/MainPage/VMain.vue";
 </script>
 
 <template>
-<div class="contents-container">
-<the-header></the-header>
+  <div class="contents-container">
+    <the-header></the-header>
 
+    <VMain />
 
-<VMain/>
-
-<the-footer></the-footer>
-</div>
-
+    <the-footer></the-footer>
+  </div>
 </template>
 
-
-
-
 <style scoped>
-.contents-container{
-    display: flex;
-    flex-direction: column;
+.contents-container {
+  display: flex;
+  flex-direction: column;
 }
 </style>
