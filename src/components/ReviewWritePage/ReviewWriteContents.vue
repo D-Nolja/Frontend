de4b376211973f49af74f8448ae9b7d81c410755
or_hyn <dhflxhdxhd@gmail.com>
2023-11-22 11:07:58 +0900

    Add : reivewWrite directory 생성 #62

---
+++ src/components/ReviewWritePage/ReviewWriteContents.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contents-container">
+    <ReviewAll />
+    <ReviewPlaces />
+  </div>
+</template>
+
+<script setup>
+import ReviewPlaces from "./ReviewWritePlaces.vue";
+import ReviewAll from "./ReviewAll.vue";
+</script>
+
+<style scoped>
+.contents-container {
+  min-height: 70vh;
+  width: 100%;
+  align-items: center;
+  margin: 90px auto;
+}
+</style>
---
+++ src/components/ReviewWritePage/ReviewWritePlaces.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="review-places-container">
+    <ReviewPlaceCard v-for="mapId in mapIdAll" :key="mapId" :mapId="mapId" />
+  </div>
+</template>
+
+<script setup>
+import ReviewPlaceCard from "./ReviewWritePlaceCard.vue";
+
+const temp = [1, 2, 3, 4];
+const mapIdAll = [];
+temp.map((item) => {
+  console.log("map" + item);
+  mapIdAll.push("map" + item);
+});
+</script>
+
+<style scoped>
+#review-places-container {
+  width: 60vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
