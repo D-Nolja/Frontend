06c0cbd832877c8d72bd070abd964854a7c4561a
or_hyn <dhflxhdxhd@gmail.com>
2023-11-22 23:21:28 +0900

    Feat : 계획페이지 이전, 다음 이동 및 카테고리 변경 구현 #20

---
+++ src/components/Common/ModalTest.vue
@@ -0,0 +1,25 @@
+<template>
+  <VButton @click="openModal"> Open Modal </VButton>
+  <ModalsContainer />
+</template>
+
+<script setup>
+import { useModal } from "vue-final-modal";
+import VModal from "./VModal.vue";
+
+const { open, close } = useModal();
+
+// 모달 열기 함수
+function openModal() {
+  open({
+    component: VModal,
+    props: {
+      title: "Hello World!",
+      onClose: close,
+    },
+    slots: {
+      default: "<p>ModalBottom: The content of the modal</p>",
+    },
+  });
+}
+</script>
